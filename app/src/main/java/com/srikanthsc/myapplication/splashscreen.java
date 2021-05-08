ae5f6decfc306e6315c1287dc4b72af081ecde44
projet <srikanthcollaty92@gmail.com>
2021-05-08 21:09:46 +0200

    ajout du spash screen et ajout d'une classe

---
+++ app/src/main/java/com/srikanthsc/myapplication/splashscreen.java
@@ -0,0 +1,29 @@
+package com.srikanthsc.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class splashscreen extends AppCompatActivity {
+    private final int SPLASH_SCREEN_TIMEOUT=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+
+           new Handler().postDelayed(new Runnable() {
+               @Override
+               public void run() {
+                   Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                   startActivity(intent);
+                   finish();
+               }
+           }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
