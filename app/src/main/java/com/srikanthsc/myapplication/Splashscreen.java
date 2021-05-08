6bc3ab6c452f920e9e3f392f88d765e744c0941a
projet <srikanthcollaty92@gmail.com>
2021-05-08 21:13:44 +0200

    ajout du splash screen et ajout d'une classe

--- app/src/main/java/com/srikanthsc/myapplication/Splashscreen.java
+++ app/src/main/java/com/srikanthsc/myapplication/Splashscreen.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 
-public class splashscreen extends AppCompatActivity {
+public class Splashscreen extends AppCompatActivity {
     private final int SPLASH_SCREEN_TIMEOUT=3000;
 
     @Override
