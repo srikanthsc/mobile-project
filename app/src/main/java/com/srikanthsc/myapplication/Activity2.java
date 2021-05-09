36faaa180281b6cbbbb587fa0cbd243c207d670f
projet <srikanthcollaty92@gmail.com>
2021-05-09 08:04:27 +0200

    modif

--- app/src/main/java/com/srikanthsc/myapplication/Activity2.java
+++ app/src/main/java/com/srikanthsc/myapplication/Activity2.java
@@ -14,10 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Activity2 extends AppCompatActivity {
     private WebView webViewwho;
