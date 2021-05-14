d362da9462262be12d8639262d4d6ec85c2d8b83
projet <srikanthcollaty92@gmail.com>
2021-05-14 20:57:03 +0200

    commit

--- app/src/main/java/com/srikanthsc/myapplication/MainActivity.java
+++ app/src/main/java/com/srikanthsc/myapplication/MainActivity.java
@@ -24,9 +24,7 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements Interface_MainActivity {
     View view, view1;
-    TextView tvCases, tvRecovered, tvCritical, tvActive, tvTodayCases, tvTotalDeaths, tvTodayDeaths, tvAffectedCountries;
-    SimpleArcLoader simpleArcLoader;
-    ScrollView scrollView;
+
     private Button button;
 
 
@@ -35,18 +33,7 @@ public class MainActivity extends AppCompatActivity implements Interface_MainAct
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvCases = findViewById(R.id.tvCases);
-        tvRecovered = findViewById(R.id.tvRecovered);
-        tvCritical = findViewById(R.id.tvCritical);
-        tvActive = findViewById(R.id.tvActive);
-        tvTodayCases = findViewById(R.id.tvTodayCases);
-        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
-        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
-        tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
 
-
-        simpleArcLoader = findViewById(R.id.loader);
-        scrollView = findViewById(R.id.scrollStats);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,63 +45,9 @@ public class MainActivity extends AppCompatActivity implements Interface_MainAct
 
         ImageView Myimageview = findViewById(R.id.imageView2);
 
-        fetchData();
 
     }
 
-    private void fetchData() {
-
-        String url = "https://corona.lmao.ninja/v2/all/";
-
-        simpleArcLoader.start();
-
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        try {
-                            JSONObject jsonObject = new JSONObject(response.toString());
-
-                            tvCases.setText(jsonObject.getString("cases"));
-                            tvRecovered.setText(jsonObject.getString("recovered"));
-                            tvCritical.setText(jsonObject.getString("critical"));
-                            tvActive.setText(jsonObject.getString("active"));
-                            tvTodayCases.setText(jsonObject.getString("todayCases"));
-                            tvTotalDeaths.setText(jsonObject.getString("deaths"));
-                            tvTodayDeaths.setText(jsonObject.getString("todayDeaths"));
-                            tvAffectedCountries.setText(jsonObject.getString("affectedCountries"));
-
-
-                            simpleArcLoader.stop();
-                            simpleArcLoader.setVisibility(View.GONE);
-                            scrollView.setVisibility(View.VISIBLE);
-
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            simpleArcLoader.stop();
-                            simpleArcLoader.setVisibility(View.GONE);
-                            scrollView.setVisibility(View.VISIBLE);
-                        }
-
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                simpleArcLoader.stop();
-                simpleArcLoader.setVisibility(View.GONE);
-                scrollView.setVisibility(View.VISIBLE);
-                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(request);
-
-
-    }
 
 
     public void goTrackCountries(View view) {
